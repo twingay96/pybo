c6af4bc9752c99e833601c25ba09adc9fd5217d4
twingay96 <flavonid37@gmail.com>
2023-06-12 00:10:01 +0900

    settings/prod.py 주석 변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,7 +2,8 @@ from .base import *
 
 ALLOWED_HOSTS = ['3.36.188.8']
 
-# python manage.py runserver --settings=config.prod
+# python manage.py runserver 0:8000 --settings=config.settings.prod
+# http://3.36.188.8:8000/
 
 '''
 서버환경에서 장고 서버를 동작할때 사용
